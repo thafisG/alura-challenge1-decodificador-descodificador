55fc93197cfe9a8225906c1cca7e831b3381f2f7
Thais Guedes <thais.guedes@ufpe.br>
2024-09-14 14:42:55 -0300

    Update script.js

--- codigos/script.js
+++ codigos/script.js
@@ -1,27 +1,85 @@
-// script.js
-function encryptText() {
-    let text = document.getElementById('inputText').value;
-    text = text.toLowerCase(); // Converter para minúsculas
+const input = document.querySelector("#input-texto");
+const btnCriptografar = document.querySelector("#btn-criptografar");
+const btnDescriptografar = document.querySelector("#btn-descriptografar");
+const mensagem = document.querySelector("#mensagem");
+const btnCopiar = document.querySelector("#btn-copiar");
 
-    const encryptedText = text
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
+//captura o id e esconde a div-aparece da tela
+document.getElementById("div-aparece").style.display = 'none';
+inputverificar();
 
-    document.getElementById('outputText').textContent = encryptedText;
+// caputura o id no momento do click e direciona para o metódo que encripta o texto
+document.getElementById('btn-criptografar').onclick = (e) => {
+  e.preventDefault();
+  const textoEncriptado = encriptar(input.value.toLowerCase());
+  mensagem.value = textoEncriptado;
+  input.value = "";
+  aparece()
 }
 
-function decryptText() {
-    let text = document.getElementById('outputText').textContent;
+// caputura o id no momento do click e direciona para o metódo que desencripta o texto
+document.getElementById('btn-descriptografar').onclick = (e) => {
+  e.preventDefault();
+  const textoDecriptado = decodificar(input.value);
+  mensagem.value = textoDecriptado;
+  input.value = "";
+  aparece()
+}
 
-    const decryptedText = text
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
+// caputura o id no momento do click e faz a validação de copiar o texto
+document.getElementById('btn-copiar').onclick = (e) => {
+  e.preventDefault();
+  const mensagem = document.querySelector("#mensagem");
+  mensagem.select();
+  navigator.clipboard.writeText(mensagem.value)
+  mensagem.value = "";
+}
 
-    document.getElementById('inputText').value = decryptedText;
+//encripta o texto
+function encriptar(stringEncriptada) {
+  let matrixCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+  stringEncriptada = stringEncriptada.toLowerCase()
+  for (let i = 0; i < matrixCode.length; i++) {
+    if (stringEncriptada.includes(matrixCode[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(matrixCode[i][0], matrixCode[i][1])
+    }
+  }
+  return stringEncriptada
+}
+
+//decodifica o texto
+function decodificar(stringDecriptada) {
+  let matrixCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+  stringDecriptada = stringDecriptada.toLowerCase()
+  for (let i = 0; i < matrixCode.length; i++) {
+    if (stringDecriptada.includes(matrixCode[i][1])) {
+      stringDecriptada = stringDecriptada.replaceAll(matrixCode[i][1], matrixCode[i][0])
+    }
+  }
+  return stringDecriptada
+}
+
+//manipula o dom para que alguns componentes apareçam e desapareçam da tela
+function aparece() {
+  document.getElementById("div-desaparece").style.display = 'none';
+  document.getElementById("div-aparece").style.display = 'block';
+}
+
+//manipula o dom para que alguns componentes apareçam e desapareçam da tela
+function home() {
+  document.getElementById("div-aparece").style.display = 'none';
+  document.getElementById("div-desaparece").style.display = 'block';
+}
+
+
+//verifica qual foi o texto digitado pelo usuário
+function inputverificar() {
+  var inputPalavra = document.querySelector("#input-texto");
+  inputPalavra.addEventListener("keypress", function (e) {
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+
+    if (keyCode > 47 && keyCode < 65) {
+      e.preventDefault();
+    }
+  });
 }
